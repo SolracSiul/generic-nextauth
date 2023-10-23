70ed89055f6fa011e73372b3d6381a2415e297ce
SolracSiul <crluis521@gmail.com>
2023-10-23 10:26:15 -0300

    replace nextauthsecret to .env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        NEXTAUTH_SECRET:"t45dViCjC1pwkXbWnJQNe221A6tkzONLzyGJbEBxht01",
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRETAUTH ?? '',
     },
 }
 
